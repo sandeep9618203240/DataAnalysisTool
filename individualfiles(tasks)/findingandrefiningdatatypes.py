4e9efb9f71ab457f759b8d711490b20bdde9a8e0
sandeep <sandeep.kolli5002@gmail.com>
2025-03-12 17:37:59 +0530

    datatypes checking is done

---
+++ individualfiles(tasks)/findingandrefiningdatatypes.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+def fun(df,num,ot):
+    print(df.head())
+    for i in range(len(num)):
+        print(df[num[i]].isna().sum())
+    for i in range(len(ot)):
+        print(df[ot[i]].isna().sum())
+    
+        
+    
+file = input("Enter the file name: ").strip('"\'')
+df = pd.read_csv(file)
+numericals=[]
+others=[]
+for col in df.columns:
+    if pd.api.types.is_numeric_dtype(df[col]):
+        numericals.append(col)
+    else:
+        others.append(col)
+fun(df,numericals,others)
+
+
+data=df
+def datatypescheck(data):
+    i=0
+    for col in data:
+        print("index is ",i,"Data Type of column", col, "is", data[col].dtype)
+        i=i+1
+    print("\n")
+    print("if any data type is not correct then enter the index and its type to change ")
+    index=int(input("enter the index"))
+
+    if index>i:
+        print("your choice is out of bounds / please re enter your choice ")
+        op=int(input("enter here:"))
+        index=op
+
+    print("enter the data type you that is correct for the column")
+    dty=input("enter here :")
+
+datatypescheck(data)
+
